824bcfeea867356c9321e2ef052031fe42f8a7ee
Adam Carter <adamcarter@jakatasalon.co.uk>
2014-08-03 10:50:52 +0100

    added to protected $dates

--- app/models/Holiday.php
+++ app/models/Holiday.php
@@ -2,6 +2,6 @@
 
 class Holiday extends Eloquent {
 	
-	protected $dates = array('request_date_from', 'request_date_to');
+	protected $dates = array('request_date_from', 'request_date_to', 'requested_on_date');
 
 }
