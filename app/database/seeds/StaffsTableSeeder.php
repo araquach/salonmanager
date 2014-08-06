a02ab24f2805a278d33b11d699fc21c7bc8443a5
Adam Carter <adamcarter@jakatasalon.co.uk>
2014-08-06 23:01:12 +0100

    changed the seed data for the modified user table

--- app/database/seeds/StaffsTableSeeder.php
+++ app/database/seeds/StaffsTableSeeder.php
@@ -13,12 +13,11 @@ class StaffsTableSeeder extends Seeder {
 		
 		Staff::create(array(
 			'person_id' => '1',
+			'user_id' => '1',
 			'iris' => '111',
 			'salon_id' => '1',
 			'staff_role_id' => '1',
 			'dob' => '1976-02-16',
-			'username' => 'adamcarter',
-			'password' => 'turnip',
 			'working_hours_week' => '40',
 			'working_hours_month' => '160',
 			'holiday_entitlement' => '28',
@@ -28,12 +27,11 @@ class StaffsTableSeeder extends Seeder {
 		
 		Staff::create(array(
 			'person_id' => '3',
+			'user_id' => '2',
 			'iris' => '333',
 			'salon_id' => '2',
 			'staff_role_id' => '2',
 			'dob' => '1984-08-30',
-			'username' => 'jimmysharpe',
-			'password' => 'parsnip',
 			'working_hours_week' => '40',
 			'working_hours_month' => '160',
 			'holiday_entitlement' => '28',
